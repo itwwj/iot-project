76c1c64c26a4a7e5141d8e44851c2a13fedf2f77
王文杰 <275236367@qq.com>
2020-09-29 18:28:18 +0800

    优化td脚本和程序

--- docs/docker/TDengine/start.sh
+++ docs/docker/TDengine/start.sh
@@ -3,8 +3,9 @@ cur_dir=`pwd`
 
 docker stop tdengine
 docker rm tdengine
-docker run --restart=always \
-    -d -v  `pwd`/logs/:/etc/taos -p 6030:6030 \
-    -v `pwd`/data/:/var/log/taos \
-    -v `pwd`/taos.cfg:/etc/taos/taos.cfg \
-    -p 6035:6035 -p 6041:6041 -p 6030-6040:6030-6040/udp tdengine/tdengine:2.0.0.0
+docker run --restart=always --name tdengine -d \
+    -v `pwd`/conf/:/etc/taos  \
+    -v `pwd`/logs/:/var/log/taos \
+    -v `pwd`/data/:/var/lib/taos \
+    -v /etc/localtime:/etc/localtime \
+    -p 6030:6030 -p 6035:6035 -p 6041:6041 -p 6030-6040:6030-6040/udp tdengine/tdengine:2.0.0.0
