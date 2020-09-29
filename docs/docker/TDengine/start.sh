d768494886dbe82a79d64fe335b1d749d3388b16
王文杰 <275236367@qq.com>
2020-09-29 08:46:31 +0800

    优化docker脚本

--- docs/docker/TDengine/start.sh
+++ docs/docker/TDengine/start.sh
@@ -3,7 +3,6 @@ cur_dir=`pwd`
 
 docker stop tdengine
 docker rm tdengine
-#docker run --restart=always --name tdengine -d -v /etc/taos:/etc/taos -p 6030:6030 -p 6035:6035 -p 6041:6041 -p 6030-6040:6030-6040/udp tdengine/tdengine:2.0.0.0
 docker run --restart=always \
     -d -v  `pwd`/logs/:/etc/taos -p 6030:6030 \
     -v `pwd`/data/:/var/log/taos \
--- docs/docker/nginx/start.sh
+++ docs/docker/nginx/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker stop nginx
+docker rm nginx
 docker run -idt -p 18883:8883 -p 28083:28083 --name nginx --restart=always \
     -v /data/projects/:/data/projects \
     -v `pwd`/conf/:/etc/nginx \
